b4496e24f5c700b9b16717bcdc8d9102fbbfd469
unknown <speedway943@wp.pl>
2022-09-25 18:13:34 +0200

    Setup connection DOM elements and added event listener

---
+++ src/main.js
@@ -0,0 +1,42 @@
+const viewElems = {};
+
+
+
+const getDOMelem = id => {
+    return document.getElementById(id);
+}
+
+const connectHTMLelems = () => { // Podłączenie elementów z HTML
+    viewElems.watherSerachView = getDOMelem('watherSerachView');
+    viewElems.mainContainer = getDOMelem('mainContainer');
+    viewElems.watherForecastView = getDOMelem('watherForecastView');
+
+    viewElems.searchInput = getDOMelem('searchInput');
+    viewElems.searchButton = getDOMelem('searchButton');
+
+    viewElems.weatherCity = getDOMelem('weatherCity');
+    viewElems.weatherIcon - getDOMelem('weatherIcon');
+
+    viewElems.weatherCurrentTemp = getDOMelem('weatherCurrentTemp');
+    viewElems.weatherMaxTemp = getDOMelem('weatherMaxTemp');
+    viewElems.weatherMinTemp = getDOMelem('weatherMinTemp');
+}
+
+const setupListeners = () => {
+    viewElems.searchInput.addEventListener('keydown', onEnterSubmit);
+    viewElems.searchButton.addEventListener('click', onClickSubmit);
+}
+
+const initializeApp = () => {
+    connectHTMLelems();
+    setupListeners();
+}
+
+const onClickSubmit = () => {
+
+}
+
+const onEnterSubmit = () => {
+
+}
+document.addEventListener('DOMContentLoaded', initializeApp);
