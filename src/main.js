bfa8d11f96679063f9a72f2cde2cc9128b76cd6e
unknown <speedway943@wp.pl>
2022-10-09 12:14:28 +0200

    Dodanie wyswietlania zawartosci API oraz dodanie animacji

--- src/main.js
+++ src/main.js
@@ -21,11 +21,13 @@ const connectHTMLelems = () => { // Podłączenie elementów z HTML
     viewElems.weatherCurrentTemp = getDOMelem('weatherCurrentTemp');
     viewElems.weatherMaxTemp = getDOMelem('weatherMaxTemp');
     viewElems.weatherMinTemp = getDOMelem('weatherMinTemp');
+    viewElems.returnToSearchBtn = getDOMelem('returnToSearchBtn');
 }
 
 const setupListeners = () => {
     viewElems.searchInput.addEventListener('keydown', onEnterSubmit);
     viewElems.searchButton.addEventListener('click', onClickSubmit);
+    viewElems.returnToSearchBtn.addEventListener('click', returnToSerach);
 }
 
 const initializeApp = () => {
@@ -34,15 +36,50 @@ const initializeApp = () => {
 }
 
 const onClickSubmit = () => {
-
+    let query = viewElems.searchInput.value;
+    getWeatherByCity(query).then(data => {
+        console.log(data);
+        switchView();
+    })
 }
 
 const onEnterSubmit = event => {
     if(event.key === 'Enter') {
+        fadeInOut();
         let query = viewElems.searchInput.value;
         getWeatherByCity(query).then(data => {
             console.log(data);
+            switchView();
+            fadeInOut()
         })
     }
+}
+
+const fadeInOut = () => {
+    if(viewElems.mainContainer.style.opacity === '1' ||     viewElems.mainContainer.style.opacity === '') {
+        viewElems.mainContainer.style.opacity = '0';
+    }
+    else {
+        viewElems.mainContainer.style.opacity = '1';
+    }
+}
+
+const switchView = () => {
+    if(viewElems.watherSerachView.style.display !== 'none') {
+        viewElems.watherSerachView.style.display = 'none';
+        viewElems.watherForecastView.style.display = 'block';
+    }
+    else {
+        viewElems.watherSerachView.style.display = 'block';
+        viewElems.watherForecastView.style.display = 'none';
+    }
+}
+
+const returnToSerach = () => {
+    fadeInOut();
+    setTimeout(() => {
+        switchView();
+        fadeInOut();
+    }, 500);
 }
 document.addEventListener('DOMContentLoaded', initializeApp);
